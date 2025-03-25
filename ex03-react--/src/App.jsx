a90e177c442ff35fb4a6dd27b22964f5c68d29ed
JuliaSilva05 <128699860+JuliaSilva05@users.noreply.github.com>
2025-03-25 21:25:40 +0000

    react simples

--- ex03-react--/src/App.jsx
+++ ex03-react--/src/App.jsx
@@ -1,35 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+// import "./App.css";
+import { Cabecalho } from "./components/Cabecalho";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [contador, setContador] = useState(0);
+  const [incremento, setIncremento] = useState(1);
+
+  function handleBtIncrementarClick() {
+    setContador(contador + 1);
+  }
+
+  function handleBtIncrementarValorClick() {
+    setContador(contador + incremento);
+  }
+
+  function handleBtLimparClick() {
+    setContador(0);
+  }
+
+  function handleIncrementoChange(evt) {
+    setIncremento(parseInt(evt.target.value));
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Cabecalho />
+      <p>Quantidades de cliques: {contador}</p>
+      <button onClick={handleBtIncrementarClick}>Incrementar</button>
+      <button onClick={handleBtLimparClick}>Limpar</button>
+      <input
+        type="number"
+        value={incremento}
+        onChange={handleIncrementoChange}
+      />
+      <button onClick={handleBtIncrementarValorClick}>Incrementar Valor</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ ex03-react--/src/components/Cabecalho.jsx
@@ -0,0 +1,9 @@
+export function Cabecalho() {
+    return (
+      <header>
+        <h1>Programação Web e Mobile (2025.1) - UNICAP</h1>
+        <h2>Júlia Silva Souto</h2>
+        <hr />
+      </header>
+    );
+  }
