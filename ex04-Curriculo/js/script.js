4cb16e5b9c5f93caa7a8ab97ff34d2fa567ce8e2
JuliaSilva05 <128699860+JuliaSilva05@users.noreply.github.com>
2025-03-28 13:45:47 +0000

    joguinho

--- ex04-Curriculo/js/script.js
+++ ex04-Curriculo/js/script.js
@@ -1,8 +1,33 @@
-function MyFunction() {
-    var x = document.getElementById("myDIV");
-    if (x.style.display === "none") {
-        x.style.display = "block";
-    } else {
+function ShowGameDesc(x) {
+    x.classList.toggle("change");
+    var x = document.getElementById("gameDesc");
+    if (x.style.display == "block") {
         x.style.display = "none";
+    } else {
+        x.style.display = "block";
     }
+}
+
+function CreateNumber(){
+    if (document.location.pathname.split("/").pop() == "jogo.html"){
+        let nums = "";
+        var novo;
+        for (let i = 0; i < 5; i++){
+            novo = Math.floor(Math.random() * 10).toString();
+            if (nums.includes(novo)){
+                i--;
+            } else {
+                nums += novo;
+            }
+        }
+        console.log(nums);
+    }
+}
+
+const nums = CreateNumber();
+
+function Tries(){
+    const inputTry=document.getElementById('tentativa').value;
+    console.log(inputTry);
+
 }
